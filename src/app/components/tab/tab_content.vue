85af0dbd37ed430df9b15dc4de8527a2b50d54b9
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2020-04-22 20:49:20 +0530

    add context menu & use paste

--- src/app/components/tab/tab_content.vue
+++ src/app/components/tab/tab_content.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="b-tab_content" ref="tabContent"></div>
+  <div>
+    <div @contextmenu.prevent="$refs.ctxMenu.open" class="b-tab_content" ref="tabContent"></div>
+    <context-menu ref="ctxMenu">
+      <li @click="paste">Paste</li>
+    </context-menu>
+  </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 import { ipcRenderer } from "electron";
 import { Terminal } from "xterm";
+import contextMenu from "vue-context-menu";
 
 import {
   IPC_EVENTS,
@@ -21,6 +27,9 @@ import {
 import { mapState } from "vuex";
 const path = require("path");
 export default Vue.extend({
+  components: {
+    contextMenu
+  },
   props: {
     id: String
   },
@@ -35,32 +44,34 @@ export default Vue.extend({
     return {};
   },
   mounted() {
-    let commandText = "";
+    this.commandText = "";
     let shouldWriteInUi = true;
     const terminal = new Terminal();
     ipcRenderer.on(IPC_EVENTS.Data, (e, { tabId, data }) => {
-      console.log("data", data);
       if (shouldWriteInUi === true && this.id === tabId) {
         terminal.write(data);
       }
     });
     terminal.onData(data => {
-      console.log("data client", data);
+      console.log("data client", data, data.length);
       if (data === "\r") {
-        debugger;
-        const commandName = commandText.split(" ")[0];
+        const commandName = this.commandText.split(" ")[0];
         const savedApp: IAppInfo = this.apps.find(
           q => q.command === commandName
         );
+        debugger;
         if (savedApp != null) {
+          const cliArgs = this.commandText.replace(commandName, "");
+          console.log("cmd", cliArgs);
           shouldWriteInUi = false;
-          for (var i = 0, len = commandText.length; i < len; i++) {
+          for (var i = 0, len = this.commandText.length; i < len; i++) {
             this.sendData("\b\u001b[K");
           }
           this.sendData("\r");
+
           this.sendData(
             // `${savedApp.target} ${path.join(savedApp.location, savedApp.main)}`
-            savedApp.commandToRun
+            `${savedApp.commandToRun} ${cliArgs}`
           );
           setTimeout(() => {
             shouldWriteInUi = true;
@@ -73,11 +84,12 @@ export default Vue.extend({
 
           // terminal.write("\b\u001b[K");
           // this.sendData(`cd ${savedApp.location} && ${savedApp.run}\r`);
+          this.commandText = "";
           return;
         }
-        commandText = "";
+        this.commandText = "";
       } else {
-        commandText += data;
+        this.commandText += data;
       }
       this.sendData(data);
     });
@@ -90,6 +102,17 @@ export default Vue.extend({
         tabId: this.id,
         data
       });
+    },
+    async paste() {
+      const copiedData = await navigator.clipboard.readText();
+      console.log("copied", copiedData);
+      if (copiedData.length > 0) {
+        // this.terminal.write(copiedData);
+        this.commandText += copiedData;
+        this.sendData(copiedData);
+      } else {
+        alert("nothing to paste");
+      }
     }
   }
 });
